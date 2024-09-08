d4657a2ef925859a6089a80713358a37c306852f
Lancus <llmaxwell21@amarillocollege.com>
2024-09-07 23:07:58 -0500

    Add project files.

---
+++ Week3-1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_1
+{
+    internal class Program
+    {
+        //Function converting INTO USD
+        static void ConvertToUSD(double amount, string fromCurrency)
+        {
+            //Checks if from EUR or JPY
+            //I used Google's stock result for exchange rates, and tried to adhere well. It is, admittedly, not perfect to the decimal.
+            if (fromCurrency == "EUR")
+            {
+                amount = (amount / 9) * 10;
+            }
+            if (fromCurrency == "JPY")
+            {
+                amount = (amount / 1000) * 7.03;
+            }
+            Console.WriteLine("Your amount in USD is: " + amount);
+        }
+        static void ConvertToEUR(double amount, string fromCurrency)
+        {
+            //Same as the others. Learned that the conversion is reversed when going the other way, which makes a lot of sense in hindsight!
+            if (fromCurrency == "USD")
+            {
+                amount = (amount / 10) * 9;
+            }
+            if (fromCurrency == "JPY")
+            {
+                amount = (amount / 1000) * 6.33;
+            }
+            Console.WriteLine("Your amount in EUR is: " + amount);
+        }
+        static void ConvertToJPY(double amount, string fromCurrency)
+        {
+            if (fromCurrency == "USD")
+            {
+                amount = (amount / 7.03) * 1000;
+            }
+            if (fromCurrency == "EUR")
+            {
+                amount = (amount / 6.33) * 1000;
+            }
+            Console.WriteLine("Your amount in JPY is: " + amount);
+        }
+
+        static void Main(string[] args)
+        {
+            //User inputs the pure amount
+            Console.WriteLine("CURRENCY CONVERTER");
+            Console.WriteLine("Enter the amount of currency: ");
+            double amount = Convert.ToDouble(Console.ReadLine());
+            //User enters fromCurrency
+            Console.WriteLine("Enter the currency that you converting from (USD, EUR, JPY): ");
+            string fromCurrency = Console.ReadLine();
+            //User inputs the currency they are converting to, which the code checks for and sends to each function
+            Console.WriteLine("Enter the currency you are converting to (USD, EUR, JPY): ");
+            string toCurrency = Console.ReadLine();
+
+            //I didn't include a failsafe for not inputting USD, EUR, or JPY, but I can if that is wanted
+            //checks for each currency, gets sent out, then is read out and user gets their amount
+            if (toCurrency == "USD")
+                ConvertToUSD(amount, fromCurrency);
+            if (toCurrency == "EUR")
+                ConvertToEUR(amount, fromCurrency);
+            if (toCurrency == "JPY")
+                ConvertToJPY(amount, fromCurrency);
+
+            Console.ReadLine();
+        }
+    }
+}
